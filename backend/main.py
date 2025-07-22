a17397f88efdf7e84c7725e9a7c15bff54d2ba8f
strikestrike <newssstrike@gmail.com>
2025-07-22 10:22:34 -0400

    fix: disable cors

--- backend/main.py
+++ backend/main.py
@@ -30,7 +30,7 @@ async def lifespan(app: FastAPI):
 # Set up CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.get_cors_origins(),
+    allow_origins=["*"],#settings.get_cors_origins(),
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
