1b14e99149f3994c8380e7619bf2f31f2dad3c98
dancodes91 <newssstrike@gmail.com>
2025-08-27 05:52:17 -0400

    Revert "fix: steve-gotit"
    This reverts commit 8436b145255e80c79ea98bec6205d57d31e381ca.

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ from app.models import Contact, ContactInterest, ContactSkill, AudioRecording, E
 from app.api.v1.api import api_router
 from database.migrate import run_initial_migration
 
-# FIX: Got it
+# FIXME:Daniel, your new passport status and the LinkedIn status should be clarified.
 # Create database tables
 Contact.metadata.create_all(bind=engine)
 
