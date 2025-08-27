c5dc04fce628dd8d12a639fcee36fed19d29f84a
dancodes91 <newssstrike@gmail.com>
2025-08-27 05:52:55 -0400

    fix: steve fixed

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ from app.models import Contact, ContactInterest, ContactSkill, AudioRecording, E
 from app.api.v1.api import api_router
 from database.migrate import run_initial_migration
 
-# FIXME:Daniel, your new passport status and the LinkedIn status should be clarified.
+# FIXME:Got it
 # Create database tables
 Contact.metadata.create_all(bind=engine)
 
